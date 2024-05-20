d7812691ca36fda99ece910a5f33c643662f9aa8
Kelly McGuffog <kellymcguffog@gmail.com>
2024-05-20 16:01:38 -0400

    solved badges and schedules lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,23 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(badge_maker(name))
+    return badges
 
 def assign_rooms(names):
-    return None
+    new_list = []
+    for index, name in enumerate(names):
+        room_assignments = f"Hello, {name}! You'll be assigned to room {index + 1}!"
+        new_list.append(room_assignments)
+    return new_list
 
 def printer(names):
-    return None
+    for name in names:
+        print(badge_maker(name))
+
+    room_messages = assign_rooms(names)
+    for message in room_messages:
+        print(message)
